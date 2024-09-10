45c0f0f3e228853f4a173cb4e61bfbdbfd07690e
syhrndraa <syahrindradr@gmail.com>
2024-09-10 18:12:52 +0700

    update route participant refresh token

--- app/api/v1/participantRefreshToken/router.js
+++ app/api/v1/participantRefreshToken/router.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express();
 const { index } = require('./controller');
 
-router.get('/refresh-token/:refreshToken/:email', index);
+router.get('/participant-refresh-token/:refreshToken/:email', index);
 
 module.exports = router;
